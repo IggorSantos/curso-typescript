b5ab61a82aee8d3c7893028fae4abca61d15e2cd
Igor Santos <igor.sagitarius@gmail.com>
2021-12-11 15:56:13 -0300

    Correções nos arquivos

--- ex-01/index.ts
+++ ex-01/index.ts
@@ -1,11 +1,11 @@
-const input1 = document.getElementById('valor-1') as HTMLElement
-const input2 = document.getElementById('valor-2') as HTMLElement
-const button = document.getElementById('button')
+const input1 = document.getElementById("valor-1") as HTMLInputElement;
+const input2 = document.getElementById('valor-2') as HTMLInputElement;
+const button = document.getElementById('button');
 
-function sum(a,b){
+function sum(a: number,b: number){
     return a + b
 }
 
 button.addEventListener("click", function(){
-    console.log(sum(input1.value,input2.value))
+    console.log(sum(Number(input1.value),Number(input2.value)))
 })
